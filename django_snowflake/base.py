abd0602b0410407a4a934eaeafcc3d7bc71b102a
punitchauhan771 <55775479+punitchauhan771@users.noreply.github.com>
2024-09-09 13:28:26 +0530

    Update base.py

--- django_snowflake/base.py
+++ django_snowflake/base.py
@@ -152,7 +152,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     @async_unsafe
     def get_new_connection(self, conn_params):
         try:
-           self.connection = Database.connect(**conn_params)
+            self.connection = Database.connect(**conn_params)
             with self.connection as conn:
                 with conn.cursor() as cur:
                     print(cur.execute("SELECT 'successfully connected to '||current_account();").fetchall())
